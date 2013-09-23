fce4ee984b93f46059fac40e49a55b52e893ebcf
Jonathan Burnhams <jburnhams@gmail.com>
2013-09-23 22:11:09 +0100

    Started wall enclosed floor

--- src/main/java/org/burnhams/flooring/floors/MultiLengthFloor.java
+++ src/main/java/org/burnhams/flooring/floors/MultiLengthFloor.java
@@ -93,7 +93,6 @@ public class MultiLengthFloor implements Floor {
 
     private void drawLine(Graphics graphics, double xMultiple, double yMultiple, int x1, int y1, int x2, int y2) {
         graphics.drawLine((int)Math.round(xMultiple*x1), (int)Math.round(yMultiple*y1), (int)Math.round(xMultiple*x2), (int)Math.round(yMultiple*y2));
-
     }
 
     @Override
